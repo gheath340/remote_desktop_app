911b58ba7bf01be86e3504f6ee894967d0aa9bdf
Garrett Heath <heathsmail0@gmail.com>
2025-10-01 10:28:36 -0600

    created client message_type_handlers.rs

---
+++ client/src/message_type_handlers.rs
@@ -0,0 +1,109 @@
+use std::error::Error;
+
+pub fn handle_text(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Text message: {:?}", String::from_utf8_lossy(payload));
+
+    Ok(())
+}
+
+pub fn handle_connect(_payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Client connected (connect message)");
+
+    Ok(())
+}
+
+pub fn handle_disconnect(_payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Client requested disconnect");
+
+    Ok(())
+}
+
+pub fn handle_error(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Error message: {:?}", String::from_utf8_lossy(payload));
+
+    Ok(())
+}
+
+pub fn handle_frame_full<T: Write>(stream: &mut T) -> Result<(), Box<dyn Error>> {
+    println!("Full frame received: {} bytes", payload.len());
+
+    Ok(())
+}
+
+pub fn handle_frame_delta(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Frame delta received: {} bytes", payload.len());
+
+    Ok(())
+}
+
+pub fn handle_cursor_shape(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Cursor shape update: {} bytes", payload.len());
+
+    Ok(())
+}
+
+pub fn handle_cursor_pos(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    if payload.len() == 8 {
+        let x = u32::from_be_bytes(payload[0..4].try_into().unwrap());
+        let y = u32::from_be_bytes(payload[4..8].try_into().unwrap());
+        println!("Cursor moved to ({x}, {y})");
+    } else {
+        println!("Invalid cursor pos payload");
+    }
+
+    Ok(())
+}
+
+pub fn handle_resize(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    if payload.len() == 8 {
+        let w = u32::from_be_bytes(payload[0..4].try_into().unwrap());
+        let h = u32::from_be_bytes(payload[4..8].try_into().unwrap());
+        println!("Resize request: {w}x{h}");
+    } else {
+        println!("Invalid resize payload");
+    }
+
+    Ok(())
+}
+
+pub fn handle_key_down(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Key down: {:?}", payload);
+
+    Ok(())
+}
+
+pub fn handle_key_up(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Key up: {:?}", payload);
+
+    Ok(())
+}
+
+pub fn handle_mouse_move(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Mouse move: {:?}", payload);
+
+    Ok(())
+}
+
+pub fn handle_mouse_down(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Mouse down: {:?}", payload);
+
+    Ok(())
+}
+
+pub fn handle_mouse_up(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Mouse up: {:?}", payload);
+
+    Ok(())
+}
+
+pub fn handle_mouse_scroll(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Mouse scroll: {:?}", payload);
+
+    Ok(())
+}
+
+pub fn handle_clipboard(payload: &[u8]) -> Result<(), Box<dyn Error>>  {
+    println!("Clipboard data: {:?}", String::from_utf8_lossy(payload));
+
+    Ok(())
+}
