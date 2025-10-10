6db201673862c3347fc163cd96400ad776cb7e84
Garrett Heath <heathsmail0@gmail.com>
2025-10-10 11:57:51 -0600

    allowed connection when server is running on both localhost and vm

--- server/src/tls.rs
+++ server/src/tls.rs
@@ -5,17 +5,22 @@ use rustls::pki_types::{CertificateDer, PrivateKeyDer};
 use std::error::Error;
 
 pub fn load_server_config() -> Result<Arc<ServerConfig>, Box<dyn Error>> {
+    //get cert and key
+    let local_cert_file = &mut BufReader::new(File::open("../certs/server.crt")?);
     let cert_file = &mut BufReader::new(File::open("../certs/server.crt")?);
     let key_file = &mut BufReader::new(File::open("../certs/server.key")?);
 
+    //get certs
     let cert_chain: Vec<CertificateDer<'static>> = certs(cert_file)
         .collect::<Result<_,_>>()?;
 
+    //get keys
     let mut keys: Vec<PrivateKeyDer<'static>> = pkcs8_private_keys(key_file)
         .map(|res| res.map(PrivateKeyDer::from))
         .collect::<Result<_,_>>()?;
     let key = keys.remove(0);
 
+    //build config
     let cfg = ServerConfig::builder()
         .with_no_client_auth()
         .with_single_cert(cert_chain, key)?;
